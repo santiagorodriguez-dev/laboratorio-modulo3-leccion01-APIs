1d0ddfadd6b96400d3db2bc023b6806fcf0b5171
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-15 14:55:03 +0200

    ok

--- src/soporte.py
+++ src/soporte.py
@@ -9,18 +9,6 @@ import requests # type: ignore
 lista_municipios = ['acebeda-la', 'ajalvir', 'alameda-del-valle', 'alamo-el', 'alcala-de-henares', 'alcobendas', 'alcorcon', 'aldea-del-fresno', 'algete', 'alpedrete', 'ambite', 'anchuelo', 'aranjuez', 'arganda-del-rey', 'arroyomolinos', 'atazar-el', 'batres', 'becerril-de-la-sierra', 'belmonte-de-tajo', 'berrueco-el', 'berzosa-del-lozoya', 'boadilla-del-monte', 'boalo-el', 'braojos', 'brea-de-tajo', 'brunete', 'buitrago-del-lozoya', 'bustarviejo', 'cabanillas-de-la-sierra', 'cabrera-la', 'cadalso-de-los-vidrios', 'camarma-de-esteruelas', 'campo-real', 'canencia', 'carabana', 'casarrubuelos', 'cenicientos', 'cercedilla', 'cervera-de-buitrago', 'chapineria', 'chinchon', 'ciempozuelos', 'cobena', 'collado-mediano', 'collado-villalba', 'colmenar-del-arroyo', 'colmenar-de-oreja', 'colmenarejo', 'colmenar-viejo', 'corpa', 'coslada', 'cubas-de-la-sagra', 'daganzo-de-arriba', 'escorial-el', 'estremera', 'fresnedillas-de-la-oliva', 'fresno-de-torote', 'fuenlabrada', 'fuente-el-saz-de-jarama', 'fuentiduena-de-tajo', 'galapagar', 'garganta-de-los-montes', 'gargantilla-del-lozoya-y-pinilla-de-buitrago', 'gascones', 'getafe', 'grinon', 'guadalix-de-la-sierra', 'guadarrama', 'hiruela-la', 'horcajo-de-la-sierra-aoslos', 'horcajuelo-de-la-sierra', 'hoyo-de-manzanares', 'humanes-de-madrid', 'leganes', 'loeches', 'lozoya', 'lozoyuela-navas-sieteiglesias', 'madarcos', 'madrid', 'majadahonda', 'manzanares-el-real', 'meco', 'mejorada-del-campo', 'miraflores-de-la-sierra', 'molar-el', 'molinos-los', 'montejo-de-la-sierra', 'moraleja-de-enmedio', 'moralzarzal', 'morata-de-tajuna', 'mostoles', 'navacerrada', 'navalafuente', 'navalagamella', 'navalcarnero', 'navarredonda-y-san-mames', 'navas-del-rey', 'nuevo-baztan', 'olmeda-de-las-fuentes', 'orusco-de-tajuna', 'paracuellos-de-jarama', 'parla', 'patones', 'pedrezuela', 'pelayos-de-la-presa', 'perales-de-tajuna', 'pezuela-de-las-torres', 'pinilla-del-valle', 'pinto', 'pinuecar-gandullas', 'pozuelo-de-alarcon', 'pozuelo-del-rey', 'pradena-del-rincon', 'puebla-de-la-sierra', 'puentes-viejas-manjiron', 'quijorna', 'rascafria', 'reduena', 'ribatejada', 'rivas-vaciamadrid', 'robledillo-de-la-jara', 'robledo-de-chavela', 'robregordo', 'rozas-de-madrid-las', 'rozas-de-puerto-real', 'san-agustin-del-guadalix', 'san-fernando-de-henares', 'san-lorenzo-de-el-escorial', 'san-martin-de-la-vega', 'san-martin-de-valdeiglesias', 'san-sebastian-de-los-reyes', 'santa-maria-de-la-alameda', 'santorcaz', 'santos-de-la-humosa-los', 'serna-del-monte-la', 'serranillos-del-valle', 'sevilla-la-nueva', 'somosierra', 'soto-del-real', 'talamanca-de-jarama', 'tielmes', 'titulcia', 'torrejon-de-ardoz', 'torrejon-de-la-calzada', 'torrejon-de-velasco', 'torrelaguna', 'torrelodones', 'torremocha-de-jarama', 'torres-de-la-alameda', 'tres-cantos', 'valdaracete', 'valdeavero', 'valdelaguna', 'valdemanco', 'valdemaqueda', 'valdemorillo', 'valdemoro', 'valdeolmos-alalpardo', 'valdepielagos', 'valdetorres-de-jarama', 'valdilecha', 'valverde-de-alcala', 'velilla-de-san-antonio', 'vellon-el', 'venturada', 'villaconejos', 'villa-del-prado', 'villalbilla', 'villamanrique-de-tajo', 'villamanta', 'villamantilla', 'villanueva-de-la-canada', 'villanueva-del-pardillo', 'villanueva-de-perales', 'villar-del-olmo', 'villarejo-de-salvanes', 'villaviciosa-de-odon', 'villavieja-del-lozoya', 'zarzalejo']
 
 """
-    Obtiene las coordenadas geográficas (latitud y longitud) de una lista de ubicaciones.
-
-    Esta función toma una lista de nombres de ubicaciones y utiliza la biblioteca Geopy para
-    obtener sus coordenadas geográficas. Utiliza un limitador de tasa para evitar exceder
-    los límites de solicitudes al servicio de geocodificación.
-
-    Args:
-        datos_input (list): Una lista de cadenas, donde cada cadena representa una ubicación (por ejemplo, nombre de un municipio).
-
-    Returns:
-        pandas.DataFrame: Un DataFrame con las columnas 'municipio', 'latitud' y 'longitud',
-        donde cada fila corresponde a las coordenadas geográficas de cada ubicación en la lista de entrada.
 """
 def obtener_coordenadas(datos_input):
 
@@ -46,9 +34,26 @@ def obtener_coordenadas(datos_input):
 
     return df
 
+def extract_values_categoria(dictionary):
+    id_categoria = dictionary[0]['id']
+    categoria = dictionary[0]['name']
+    return id_categoria, categoria
+
+def extract_values_datos(dictionary):
+    direccion = dictionary['formatted_address']
+    return direccion
+
+def extract_values_position(dictionary):
+    main = dictionary['main']
+    latitude = main['latitude']
+    longitude = main['longitude']
+
+    return latitude, longitude
+
+
 def get_data_places(municipio, categorias, distancia ,latitud, longitud, token):
     
-    url = f"https://api.foursquare.com/v3/places/search?ll={latitud}%2C{longitud}&radius={distancia}&categories={categorias}&fields=fsq_id%2Cname%2Clocation%2Ccategories%2Cdistance&sort=DISTANCE&limit=50"
+    url = f"https://api.foursquare.com/v3/places/search?ll={latitud}%2C{longitud}&radius={distancia}&categories={categorias}&fields=fsq_id%2Cname%2Clocation%2Ccategories%2Cdistance%2Cgeocodes&sort=DISTANCE&limit=50"
     
     headers = {
         "accept": "application/json",
@@ -61,6 +66,12 @@ def get_data_places(municipio, categorias, distancia ,latitud, longitud, token):
     df = pd.DataFrame(resultado['results'])
     df['municipio']=pd.DataFrame(columns=["pueblos"])
     df['municipio'] = municipio
+
+    df[['id_categoria', 'categoria']] = df['categories'].apply(lambda x: pd.Series(extract_values_categoria(x)))
+    df['direccion'] = df['location'].apply(lambda x: pd.Series(extract_values_datos(x)))
+    df[['latitud', 'longitud']] = df['geocodes'].apply(lambda x: pd.Series(extract_values_position(x)))
+
+    df = df.drop(['categories', 'location','geocodes'], axis=1)
 
     return df
  
